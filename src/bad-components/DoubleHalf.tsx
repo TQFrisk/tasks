00284a9befac736e371839e8e97710916a657c25
TQFrisk <tqfrisk@gmail.com>
2026-03-30 21:34:38 -0400

    Fix DoubleHalf and removed old tests

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,40 +1,26 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
-
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
 
 export function DoubleHalf(): React.JSX.Element {
+    const [value, setValue] = useState<number>(10);
+
     return (
         <div>
-            <h3>Double Half</h3>
-            <div>
-                The current value is: <span>{dhValue}</span>
-            </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <div>{value}</div>
+            <Button
+                onClick={() => {
+                    setValue(value * 2);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setValue(value / 2);
+                }}
+            >
+                Halve
+            </Button>
         </div>
     );
 }
